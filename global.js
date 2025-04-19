593c7d51acea649f7b1e96b6c26fde8323b6bdf0
SC450 <shc068@ucsd.edu>
2025-04-18 20:24:29 -0700

    Update global.js

--- global.js
+++ global.js
@@ -17,9 +17,9 @@ console.log('IT’S ALIVE!');
 document.addEventListener('DOMContentLoaded', () => {
     let pages = [
         {url: "../", title: "Home"},
-        {url: "../projects/", title: "Projects"},
-        {url: "../contact/", title: "Contact"},
-        {url: "../resume/", title: "Resume"},
+        {url: "../portfolio/projects/", title: "Projects"},
+        {url: "../portfolio/contact/", title: "Contact"},
+        {url: "../portfolio/resume/", title: "Resume"},
         {url: "https://github.com/SC450", title: "GitHub"}
     ];
 
